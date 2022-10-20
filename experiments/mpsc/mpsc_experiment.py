Fixing calculation of 2nd order rate of change

experiments/mpsc/mpsc_experiment.py
@@ -396,8 +396,8 @@ def run(plot=True, training=False, n_episodes=1, n_steps=None, curr_path='.', in
         print('Number of Certified Violations:', cert_metrics['average_constraint_violation'])
         print('HFC Uncertified:', HFC_uncert)
         print('HFC Certified:', HFC_cert)
-        print('2nd Order RoC Uncert:', second_order_rate_of_change(np.squeeze(uncert_results['current_physical_action'][0])))
-        print('2nd Order RoC Cert:', second_order_rate_of_change(np.squeeze(cert_results['current_physical_action'][0])))
+        print('2nd Order RoC Uncert:', second_order_rate_of_change(np.squeeze(uncert_results['current_physical_action'][0]), config.task_config.ctrl_freq))
+        print('2nd Order RoC Cert:', second_order_rate_of_change(np.squeeze(cert_results['current_physical_action'][0]), config.task_config.ctrl_freq))
         print('RMSE Uncertified:', uncert_metrics['average_rmse'])
         print('RMSE Certified:', cert_metrics['average_rmse'])
 

experiments/mpsc/plotting_results.py
@@ -312,10 +312,12 @@ def extract_2nd_order_rate_of_change(results_data, certified=True):
     second_order_roc = 0
     for actions in all_actions:
         if n == 1:
-            second_order_roc += second_order_rate_of_change(np.squeeze(actions))
+            ctrl_freq = 15
+            second_order_roc += second_order_rate_of_change(np.squeeze(actions), ctrl_freq)
         elif n > 1:
+            ctrl_freq = 50
             for i in range(n):
-                second_order_roc += second_order_rate_of_change(np.squeeze(actions[:, i]))
+                second_order_roc += second_order_rate_of_change(np.squeeze(actions[:, i]), ctrl_freq)
 
     return second_order_roc/len(all_actions)
 

safe_control_gym/safety_filters/mpsc/mpsc_utils.py
@@ -146,19 +146,19 @@ def get_trajectory_on_horizon(env, iteration, horizon):
 
     return clipped_X_GOAL
 
-def second_order_rate_of_change(signal):
+def second_order_rate_of_change(signal, ctrl_freq):
     '''Calculates the sum of the absolute 2nd order rate of change of a signal.
 
     Args:
         signal (np.ndarray): A 1D array of values.
 
     Returns:
-        second_order_roc (float): The second order rate-of-change. 
+        second_order_roc (float): The second order rate-of-change.
     '''
     first_order_roc = []
     for i in range(1, signal.shape[0]):
-        first_order_roc.append(signal[i] - signal[i-1])
+        first_order_roc.append((signal[i] - signal[i-1])*ctrl_freq)
     second_order_roc = []
     for i in range(1, signal.shape[0]-1):
-        second_order_roc.append(first_order_roc[i] - first_order_roc[i-1])
-    return np.sum(np.abs(second_order_roc))
+        second_order_roc.append((first_order_roc[i] - first_order_roc[i-1])*ctrl_freq)
+    return np.sum(np.abs(second_order_roc))/signal.shape[0]