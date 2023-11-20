9f9ed3973476ec55948d276bb9dab868e4075317
pizarrob <pizarrob@v2.cluster.local>
2023-11-20 10:32:27 -0500

    Adding results with rew_state_pen at 2 and various sf_pens

--- experiments/mpsc/train_all_models.sh
+++ experiments/mpsc/train_all_models.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 for SYS in quadrotor_3D; do
-    for ALGO in sac ppo; do
+    for ALGO in ppo; do
         for TASK in track; do
-            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False #mpsf_sr_pen
-            sbatch train_model.sbatch none False False $SYS $TASK $ALGO False #none
-            sbatch train_model.sbatch none False False $SYS $TASK $ALGO True #none_cpen
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 0 #mpsf_sr_pen_0
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 1 #mpsf_sr_pen_1
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 1 #mpsf_sr_pen_3
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 10 #mpsf_sr_pen_10
+            sbatchk train_model.sbatch mpsf True True $SYS $TASK $ALGO False 25 #mpsf_sr_pen_25
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 75 #mpsf_sr_pen_75
+            sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 200 #mpsf_sr_pen_200
+            sbatch train_model.sbatch none False False $SYS $TASK $ALGO False False #none
+            sbatch train_model.sbatch none False False $SYS $TASK $ALGO True False #none_cpen
             if [ "$ALGO" == 'ppo' ]; then
                 sbatch train_model.sbatch none False False $SYS $TASK safe_explorer_ppo False #safe-ppo
                 sbatch train_model.sbatch none False False $SYS $TASK cpo False #cpo
