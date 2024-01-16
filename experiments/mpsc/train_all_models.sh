432ffa2f37b829f46d3afa820a42a4128ef7c3e7
pizarrob <pizarrob@v2.cluster.local>
2024-01-16 14:54:24 -0500

    Fixing small bug in train_all script

--- experiments/mpsc/train_all_models.sh
+++ experiments/mpsc/train_all_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for SYS in quadrotor_3D; do
-    for ALGO in sac; do
+    for ALGO in ppo; do
         for TASK in track; do
             for SEED in 42 62 821 99 4077; do # 1102 1014 14 960406 2031; do
                 sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 1 $SEED #mpsf_sr_pen_1
@@ -9,10 +9,10 @@ for SYS in quadrotor_3D; do
                 sbatch train_model.sbatch mpsf True True $SYS $TASK $ALGO False 1000 $SEED #mpsf_sr_pen_1000
                 sbatch train_model.sbatch none False False $SYS $TASK $ALGO False False $SEED #none
                 sbatch train_model.sbatch none False False $SYS $TASK $ALGO True False $SEED #none_cpen
-                # if [ "$ALGO" == 'ppo' ]; then
-                #     sbatch train_model.sbatch none False False $SYS $TASK safe_explorer_ppo False #safe-ppo
-                #     sbatch train_model.sbatch none False False $SYS $TASK cpo False #cpo
-                # fi
+                if [ "$ALGO" == 'ppo' ]; then
+                    sbatch train_model.sbatch none False False $SYS $TASK safe_explorer_ppo False False $SEED #safe-ppo
+                    sbatch train_model.sbatch none False False $SYS $TASK cpo False False $SEED #cpo
+                fi
             done
         done
     done
