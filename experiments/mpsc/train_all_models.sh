162075e4960530c621ca90d747009bfc61b6b45a
Federico Pizarro Bejarano <federico.pizarrobejarano@mail.utoronto.ca>
2024-11-18 09:42:36 -0500

    Minor update

--- experiments/mpsc/train_all_models.sh
+++ experiments/mpsc/train_all_models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./train_model.sbatch True 1 1
+sbatch train_model.sbatch True 1 1
 for MPSC_COST_HORIZON in 2 5 10 20; do
     for DECAY_FACTOR in 0.25 0.5 0.75 1; do
-        ./train_model.sbatch True  $MPSC_COST_HORIZON $DECAY_FACTOR
-        # ./train_model.sbatch False $MPSC_COST_HORIZON $DECAY_FACTOR
+        sbatch train_model.sbatch True  $MPSC_COST_HORIZON $DECAY_FACTOR
+        sbatch train_model.sbatch False $MPSC_COST_HORIZON $DECAY_FACTOR
     done
 done
