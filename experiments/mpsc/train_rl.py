68fa67691e5bc3694fcec017057d4ccd69bcd4aa
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2025-03-28 10:00:45 -0400

    Updating stabilization training

--- experiments/mpsc/train_rl.py
+++ experiments/mpsc/train_rl.py
@@ -21,7 +21,7 @@ def train():
 
     TODO: Add restore functionality
     '''
-    # Create the configuration dictionary.
+    # Create the configuration dictionary
     fac = ConfigFactory()
     config = fac.merge()
     config.algo_config['training'] = True
@@ -36,14 +36,14 @@ def train():
     set_seed_from_config(config)
     set_device_from_config(config)
 
-    # Define function to create task/env.
+    # Define function to create task/env
     env_func = partial(make,
                        config.task,
                        output_dir=config.output_dir,
                        **config.task_config
                        )
 
-    # Create the controller/control_agent.
+    # Create the controller/control_agent
     ctrl = make(config.algo,
                 env_func,
                 checkpoint_path=os.path.join(config.output_dir, 'model_latest.pt'),
@@ -51,7 +51,7 @@ def train():
                 **config.algo_config)
     ctrl.reset()
 
-    # Setup MPSC.
+    # Setup MPSC
     if config.algo in ['ppo', 'sac']:
         ctrl.model_name = config.output_dir.split('/')[-2]
         safety_filter = make(config.safety_filter,
@@ -69,15 +69,18 @@ def train():
 
         ctrl.safety_filter = safety_filter
 
-    # Training.
-    start_time = time.time()
-    ctrl.learn()
-    config['logging'] = {'total_learning_time': time.time() - start_time}
-    ctrl.close()
-    print('Training done.')
-
+    # Save the config
     with open(os.path.join(config.output_dir, 'config.yaml'), 'w', encoding='UTF-8') as file:
         yaml.dump(munch.unmunchify(config), file, default_flow_style=False)
+
+    # Training
+    start_time = time.time()
+    ctrl.learn()
+    ctrl.close()
+
+    with open(os.path.join(config.output_dir, 'training_time.txt'), 'w', encoding='UTF-8') as f:
+        f.write('Total training time: ' + str(time.time() - start_time))
+    print('Training done.')
 
     make_plots(config)
 
@@ -89,7 +92,7 @@ def make_plots(config):
             the experiment folder containing the logs.
         * save figures under `dir_path/plots/`.
     '''
-    # Define source and target log locations.
+    # Define source and target log locations
     log_dir = os.path.join(config.output_dir, 'logs')
     plot_dir = os.path.join(config.output_dir, 'plots')
     mkdirs(plot_dir)
